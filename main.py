ef759ef678c880f48f794625f9c41aadfa25581d
Devin Hero <>
2024-04-18 22:03:06 -0700

    Further UI work, create icon generator for visualBoard cells

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Meinsweeper
+# (C) Devin Hero 2024
+
 from enum import Enum
 import os
 
@@ -11,7 +13,7 @@ class StaticBoardCellContent(Enum):
     EMPTY = 0
     BOMB = 1
 
-boardWidth, boardHeight = (18,12)
+boardWidth, boardHeight = (8,8)
 bombCount = 10
 endGame = False
 
@@ -28,6 +30,8 @@ interactBoard = []
 ##### 4: staticBoard holds bombs only, visualBoard is generated from scratch every time
 #### For starting: 4: staticBoard holds bombs only. VisualBoard re-created each time
 
+#### Hey me: what if the board reshuffled bombs every X flags placed, preserving correct+incorrect flags and re-hiding all non-flagged squares?
+#### What if it triggered every X actions, flag or reveal?
 
 def main():
     #initialize both boards
@@ -59,15 +63,33 @@ def InitializeStaticBoard():
     
     # Randomly pick cell; if empty, fill it and decrease
     # Dummy vals for testing purposes!
+    staticBoard[1][1] = StaticBoardCellContent.BOMB
     staticBoard[1][4] = StaticBoardCellContent.BOMB
     staticBoard[1][5] = StaticBoardCellContent.BOMB
     staticBoard[1][6] = StaticBoardCellContent.BOMB
     staticBoard[5][5] = StaticBoardCellContent.BOMB
+    staticBoard[3][7] = StaticBoardCellContent.BOMB
+
+    staticBoard[0][0] = StaticBoardCellContent.BOMB
+    staticBoard[7][0] = StaticBoardCellContent.BOMB
+    staticBoard[0][7] = StaticBoardCellContent.BOMB
+    staticBoard[7][7] = StaticBoardCellContent.BOMB
+
     return staticBoard
 
 
 def InitializeInteractBoard():
     interactBoard = [[InteractBoardCellState.HIDDEN for i in range(boardWidth)] for j in range(boardHeight)]
+    # interactBoard = [[InteractBoardCellState.CLICKED for i in range(boardWidth)] for j in range(boardHeight)]
+    interactBoard[0][0] = InteractBoardCellState.FLAGGED
+    interactBoard[1][0] = InteractBoardCellState.FLAGGED
+    interactBoard[0][1] = InteractBoardCellState.CLICKED
+    interactBoard[1][1] = InteractBoardCellState.CLICKED
+
+    interactBoard[4][7] = InteractBoardCellState.CLICKED
+    interactBoard[5][7] = InteractBoardCellState.CLICKED
+    
+
     return interactBoard
 
 def PromptUserAction():
@@ -76,15 +98,15 @@ def PromptUserAction():
     #call appropriate of Reveal or Flag functions as.... appropriate
     #
 
-def RevealInteractBoardCell():
+def RevealCellInteractBoard():
     wowow = 2
     ## if bomb: update stateBoard cell, update endGame
     ## if number: update stateBoard cell
     ## if empty: update stateBoard, recurse upon all VALID surrounding cells
-    ### most elegant way to do this? Blind recurion 
+    ### most elegant way to do this? Blind recurion will have a good amount of redundant checks
     
 
-def FlagInteractBoardCell():
+def FlagCellInteractBoard():
     placeholder = 0
     # if hidden: change to flag
     ## if bomb space, decrease remaining bomb counter
@@ -140,26 +162,19 @@ def UpdateVisualBoard():
 
 ## Board Print Actions
 
-def PrintRevealedVisualBoard():
+def PrintVisualBoard(isGameEnd=False):
     #For Game Win/Lose to show entire board
+    PrintBoardTopBorder()
     for row in range(boardHeight):
+        print('|', end='')
         for column in range(boardWidth):
-            placingholder = 1
-            #Treat Hidden as Revealed
-            #What to display for incorrectly flagged tile?
-            #
+            icon = GetVisualBoardCellIcon(row, column, isGameEnd)
+            # icon = '_' if icon == ' ' else icon
+            print(f' {icon} |', end='')
+        print()
+    PrintBoardBottomBorder()
+    PrintXAxisLabels()
 
-def PrintVisualBoard():
-    global visualBoard
-    #print out board visually
-    # print(' ', end='')
-    # for x in range(boardWidth):
-    #     print('___ ', end='')
-    # print()
-    # for row in staticBoard:
-    #     print('|', end='')
-    #     for cell in row:
-    #         wow = 0
     
     #   A   B   C
     #  ___________
@@ -167,14 +182,16 @@ def PrintVisualBoard():
     # | 1 | 2 | 1 |   2
     # | * | 1 |   |   3
 
-
+# Doesn't show numbers or any interaction states, just bombs; primarily useful for debugging
 def PrintStaticBoard():
     global staticBoard
     PrintBoardTopBorder()
     for row in range(boardHeight):
         print('|', end='')
         for column in range(boardWidth):
-            print(f' {GetStaticBoardCellIcon(staticBoard[row][column])} |', end='')
+            icon = GetStaticBoardCellIcon(staticBoard[row][column])
+            icon = '_' if icon == ' ' else icon
+            print(f'_{icon}_|', end='')
         print()
     PrintBoardBottomBorder()
     PrintXAxisLabels()
@@ -190,7 +207,7 @@ def PrintBoardTopBorder():
 def PrintBoardBottomBorder():
     print(' ', end='')
     for x in range(boardWidth):
-        print('‾‾‾ ', end='')
+        print('--- ', end='')
     print()
 
 def PrintXAxisLabels():
@@ -203,11 +220,37 @@ def PrintXAxisLabels():
             #What are you doing at this point? How big is your screen??
             label = label[1:]
         print(label, end='')
+    print()
     
 def GetStaticBoardCellIcon(cell):
     return '*' if cell == StaticBoardCellContent.BOMB else ' '
 
+def GetVisualBoardCellIcon(yRow, xColumn, isGameEnd=False):
+    localBombCount = 0
+    if(interactBoard[yRow][xColumn] == InteractBoardCellState.HIDDEN):
+        return ' '
 
+    if(interactBoard[yRow][xColumn] == InteractBoardCellState.FLAGGED):
+        if(isGameEnd and staticBoard[yRow][xColumn] == StaticBoardCellContent.EMPTY):
+            return '#'
+        else:
+            return 'F'
+
+    if(staticBoard[yRow][xColumn] == StaticBoardCellContent.BOMB):
+        return '*' if interactBoard[yRow][xColumn] != InteractBoardCellState.CLICKED else 'X'
+
+    #Anything else clicked by now is a number
+    for y in range(yRow-1, yRow+2):
+        for x in range(xColumn-1, xColumn+2):
+            validCoordinates = [
+                x >= 0,
+                y >= 0,
+                x < boardWidth,
+                y < boardHeight]
+            
+            if (all(validCoordinates) and staticBoard[y][x] == StaticBoardCellContent.BOMB):
+                localBombCount += 1
+    return str(localBombCount)
 
 
 
@@ -217,3 +260,5 @@ def GetStaticBoardCellIcon(cell):
 staticBoard = InitializeStaticBoard()
 interactBoard = InitializeInteractBoard()
 PrintStaticBoard()
+PrintVisualBoard()
+PrintVisualBoard(True)
